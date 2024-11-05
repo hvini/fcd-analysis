9f06f58461b5a0265dfe882d8166be9538ee0c0c
vinicius <vhpcavalcanti@outlook.com>
2024-11-05 14:52:58 -0300

    removes unfinished scans on pre processing

--- pre_processing.sh
+++ pre_processing.sh
@@ -2,15 +2,25 @@
 
 BASE_DIR=$(pwd)
 DATASET_DIR="$BASE_DIR/ds004199"
-
+SUBJECTS_DIR="$FREESURFER_HOME/subjects"
+pending_subjects=()
+commands=()
 jobs=24
+
 subjects=($(find "$DATASET_DIR" -type d -name 'sub-*' -exec basename {} \;))
 
-commands=()
-
 for subject in "${subjects[@]}"; do
+    log_file="$SUBJECTS_DIR/$subject/scripts/recon-all.log"
+    last_line=$(tail -1 "$log_file" 2>/dev/null)
+    if [[ "$last_line" != *"finished without error"* ]]; then
+        rm -rf "$SUBJECTS_DIR/$subject"
+        pending_subjects+=("$subject")
+    fi
+done
+
+for subject in "${pending_subjects[@]}"; do
     scan=$(find "$DATASET_DIR/$subject/anat" -name '*_T1w.nii.gz' | head -n 1)
-    
+
     if [[ -n $scan ]]; then
         commands+=("recon-all -s \"$subject\" -i \"$scan\" -all -qcache")
     else
